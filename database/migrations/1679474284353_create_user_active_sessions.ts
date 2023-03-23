0728897358e8c30d7917fcc4dab06a705abff1ca
ferdinandosp <ferdinando.perkasa@wintermar.com>
2023-03-23 09:11:49 +0700

    use pg instead of sqlite

---
+++ database/migrations/1679474284353_create_user_active_sessions.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'user_active_sessions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('token').notNullable().unique()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
