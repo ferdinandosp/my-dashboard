d9438d11e83f9fb1c61af034b0b3644d3c17eb69
ferdinandosp <ferdinando.perkasa@wintermar.com>
2023-03-23 20:01:20 +0700

    fix swagger issue

--- config/swagger.ts
+++ config/swagger.ts
@@ -26,5 +26,5 @@ export default {
 		basePath: '/'
 	},
 	mode: process.env.NODE_ENV === 'production' ? 'PRODUCTION' : 'RUNTIME',
-  specFilePath: 'docs/swagger.json'
+  specFilePath: 'public/docs/swagger.json'
 } as SwaggerConfig
