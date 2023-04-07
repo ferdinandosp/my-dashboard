a031b908a95f5950c259c5df87c8f0e851d2a8de
ferdinandosp <ferdinando.perkasa@wintermar.com>
2023-04-07 20:49:55 +0700

    API to get logged in user's profile.

--- app/Controllers/Http/Profile/GetUserProfileController.ts
+++ app/Controllers/Http/Profile/GetUserProfileController.ts
@@ -2,24 +2,52 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class GetUserProfileController {
+  /**
+  * @swagger
+  * /profile:
+  *   get:
+  *     tags:
+  *       - Users
+  *     summary: Get user profile
+  *     produces:
+  *       - application/json
+  *     responses:
+  *       200:
+  *         description: Success
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: object
+  *               properties:
+  *                 email:
+  *                   type: string
+  *                   example: 'example@email.com'
+  *                 name:
+  *                   type: string
+  *                   example: 'John Doe'
+  *       401:
+  *         description: Unauthorized
+  */
   public async handle(ctx: HttpContextContract) {
-    const { auth, response, view } = ctx
+    const { auth, response } = ctx
 
-    const user: User = await auth.authenticate()
+    let user: User
 
-    if (!user) {
-      return response.redirect('/login')
+    try {
+      user = await auth.authenticate()
+    } catch {
+      return response.unauthorized({ message: 'Unauthorized' })
     }
 
-    return view.render('profile', {
-      email:
-      {
-        value: user.email
-      },
-      name:
-      {
-        value: user.name
-      }
-    })
+    if (!user) {
+      return response.unauthorized({ message: 'Unauthorized' })
+    }
+
+    const userProfileResponse: UserProfileResponse = {
+      email: user.email,
+      name: user.name
+    }
+
+    return response.ok(userProfileResponse)
   }
 }
